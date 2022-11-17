6a8b2c499102562be8a89c4e4b4f112cb4368df8
Ron <83615736+RonTsarevsky@users.noreply.github.com>
2022-11-17 19:05:12 +0200

    Update WebApp-Clone.ps1

--- WebApp-Clone.ps1
+++ WebApp-Clone.ps1
@@ -1,8 +1,8 @@
 $source_asp = Get-AzAppServicePlan -ResourceGroupName "cloneproj" -Name "p3v2"
 $get_asp = Get-AzAppServicePlan -ResourceGroupName "cloneproj" -Name "clone-app-service-plan"
 $apps = Get-AzWebApp | Where-Object { $_.ServerFarmId -eq $source_asp.Id }
-$pfxPath = "/home/ron/certificate.pfx"
-$pfxPassword = "ron13121997"
+$pfxPath = ""
+$pfxPassword = ""
 $pfxPathAzri = ""   
 $pfxPasswordAzri = ""
 $AzriWebApp = Get-AzWebApp -ResourceGroupName "" -Name "" | Where-Object { $_.ServerFarmId -eq $source_asp.Id }
